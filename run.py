36659231ad965231ce761ccffda150c2b484eb50
Garath Davis <garath.davis.gti@gmail.com>
2018-11-26 14:44:07 +0000

    Initial Commit - Set up bootstrap template, working with scss, basic views and template

---
+++ run.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    title = "Home Page"
+    description = "Spanish Word Game"
+    return render_template("index.html", title=title, description=description)
+    
+@app.route('/about')
+def about():
+    title = "About"
+    description = "A little bit About Us"
+    return render_template("about.html", title=title, description=description)
+    
+@app.route('/contact')
+def contact():
+    title = "Contact Page"
+    description = "Get in Touch"
+    return render_template("contact.html", title=title, description=description)
+
+@app.route('/scores')
+def scores():
+    title = "High Scores"
+    description = "Highest Scores"
+    return render_template("scores.html", title=title, description=description)
+    
+    
+
+app.run(
+    host=os.getenv('IP'), 
+    port=int(os.getenv('PORT')), 
+    debug=True)
