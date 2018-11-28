62ab4587a24bc0b08f53a434bd3108ebc0eb96ec
Garath Davis <garath.davis.gti@gmail.com>
2018-11-28 08:27:27 +0000

    Added a bit of functionality for random questions but still alot to fiqure out

--- static/js/index.js
+++ static/js/index.js
@@ -4,7 +4,25 @@ $(document).ready(function() {
 
     var questions = [];
     var writing = document.getElementById("questions");
-    
+
+
+    function randomNumber(limit, callback) {
+        var array = [];
+        var min = 0;
+        for (var i = 0; i < 4; i++) {
+            var result = parseInt(Math.random() * (limit - min) + min);
+
+            for (var a = 0; a < array.length; a++) {
+                if (result === a.length) {
+                    array.splice(a, 1);
+                }
+            }
+
+            array.push(result);
+        }
+        callback(array);
+    };
+
 
     function searchList(callback) {
         var array = [];
@@ -16,22 +34,36 @@ $(document).ready(function() {
         });
     };
 
+    //Get the Questions
     function getQuestions() {
         searchList(function(response) {
-            response.forEach(function(entry) {
+
+            //Random Number Generation
+            var limit = response.length;
+            console.log("limit: " + limit);
+            randomNumber(limit, function(numbers) {
+                console.log("The random number is: " + numbers);
+
                 questions.push(`
                 <div class="row">
                  <div class="col-md-12">
-                     <h2>What is the meaning of: ${entry.Spanish}?</h2>
+                     <h2>What is the meaning of: ${response[0].Spanish}?</h2>
+                     <hr />
+                     <p>Choose one of the following:</p>
                      <ul>
-                         <l1><label><p>Its : ${entry.English}</p></label></l1>
+                         <l1><label><button class="btn btn-primary">${response[numbers[0]].English}</button></label></l1>
+                         <l1><label><button class="btn btn-primary">${response[numbers[1]].English}</button></label></l1>
+                         <l1><label><button class="btn btn-primary">${response[numbers[2]].English}</button></label></l1>
+                         <l1><label><button class="btn btn-primary">${response[numbers[3]].English}</button></label></l1>
                      </ul>
                  </div> 
                 </div>
                 `);
+
+                writing.innerHTML = questions.join('');
+
             });
-        
-            writing.innerHTML = questions.join('');
+
         });
     };
 
