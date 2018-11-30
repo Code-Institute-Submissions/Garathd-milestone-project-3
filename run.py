5b0232c54a9c100072acdf24f720302d81d1d48d
Garath Davis <garath.davis.gti@gmail.com>
2018-11-30 17:02:08 +0000

    Completely got rid of the Javascript stuff and try and go all out python approach
    q
    x

--- run.py
+++ run.py
@@ -1,12 +1,52 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
+
+counter = 0;
+
+def getQuestions():
+    with open("data/words.json", "r") as the_questions:
+        q = json.load(the_questions)
+    return q
 
 @app.route('/' , methods=["GET", "POST"])
 def index():
     title = "Home Page"
-    description = "Spanish Word Game"
+    description = "Welcome"
     return render_template("index.html", title=title, description=description)
+    
+@app.route('/questions', methods=["GET","POST"])
+def questions():
+    
+    r = getQuestions()
+    results = r[counter]
+    
+    title = "Question Game"
+    description = "Spanish Word Game"
+    if request.method == "POST":
+        return redirect("questions/{0}".format(request.form["answer"])) 
+    
+    return render_template("questions.html", title=title, description=description, results=results, counter=counter)
+    
+@app.route('/questions/<question>', methods=["GET","POST"])
+def askQuestions(question):
+    
+    r = getQuestions()
+    results = r[counter]
+    answer = question
+    title = "Question Game"
+    description = "Spanish Word Game"
+    
+    if request.method == "POST":
+        return redirect("questions") 
+
+    return render_template("answered.html",
+    title=title,
+    description=description,
+    results=results,
+    answer=answer)
             
 @app.route('/about')
 def about():
