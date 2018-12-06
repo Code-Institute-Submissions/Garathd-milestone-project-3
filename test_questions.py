502318ae072dbfd0ecc4982027a8234c95c88f55
Garath Davis <garath.davis.gti@gmail.com>
2018-12-06 13:40:14 +0000

    Code Cleanup

--- test_questions.py
+++ test_questions.py
@@ -42,23 +42,26 @@ class TestQuestions(unittest.TestCase):
         to the score.txt file and then this record is deleted once it has been found. After
         which the original score data is restored
         """
+        
         score_list = "data/score.txt"
         username = "unittest"
         score = "0x01"
         result = 0
         
+        #Setting up copy/backup of quiz scores
         backup_scores = question_functions.get_scores()
 
+        #Write test data to score.txt file
         question_functions.set_score(username, score)
         
         #Writing test data to the score.txt file
         test_score = question_functions.get_scores()
-        
-        """
-        
-        """
+
+        #Checking if test data has been added
         for sc in test_score:
             if sc == "unittest got a total of 0x01 points\n":
+                
+                #Passed the test
                 result = True
                 
                 #Delete the score.txt file
@@ -76,9 +79,11 @@ class TestQuestions(unittest.TestCase):
                             file.writelines(old_score)
                 
             else:
+                #Failed the test
                 result = False
      
         self.assertTrue(result)
+        
 
 if __name__ == "__main__":
     unittest.main()
